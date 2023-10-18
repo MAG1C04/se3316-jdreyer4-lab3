66d3dbc103d961b625743e155c4a396a194c0ff6
MAG1C04 <jdreyer4@uwo.ca>
2023-10-18 15:09:26 -0400

    feat: Implement front end template

---
+++ client/script.js
@@ -0,0 +1,34 @@
+let search = document.getElementById("superHeroSearchBar");
+
+let searchName = document.getElementById("searchName");
+let searchRace = document.getElementById("searchRace");
+let searchPublisher = document.getElementById("searchPublisher");
+let searchPower = document.getElementById("searchPower");
+
+searchName.addEventListener('click', searchByName);
+searchRace.addEventListener('click', searchByRace);
+searchPublisher.addEventListener('click', searchByPublisher);
+searchPower.addEventListener('click', searchByPower);
+
+let resultsSection = document.getElementById("results");
+
+let sortName = document.getElementById("sortByName");
+let sortRace = document.getElementById("sortByRace");
+let sortPublisher = document.getElementById("sortByPublisher");
+let sortPower = document.getElementById("sortByPower");
+
+function searchByName(){
+
+}
+
+function searchByRace(){
+    
+}
+
+function searchByPublisher(){
+    
+}
+
+function searchByPower(){
+    
+}
--- index.js
+++ index.js
@@ -4,6 +4,5 @@ const app = express();
 app.use(express.json());
 
 
-
 const port = 3000;
 app.listen(port, () => console.log(`Listening on Port ${port}...`));
